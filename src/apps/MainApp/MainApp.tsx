d494d4a3e8a2266e3d396601b466f53192028fc0
Dayem Siddiqui <dayemsiddiqui@gmail.com>
2021-10-03 19:24:52 +0200

    Voting result

--- src/apps/MainApp/MainApp.tsx
+++ src/apps/MainApp/MainApp.tsx
@@ -2,16 +2,30 @@ import React from "react";
 import LandingPageHeader from "../LandingPageApp/LandingPageHeader";
 import { CardSelector } from "./SelectedCard";
 import { VoteViewer } from "./VoteViewer";
-import { ResultViewer } from "./ResultViewer";
+import { VotingResult } from "./VotingResult";
 
 export const MainApp: React.FC<{}> = () => {
+  const winners = [
+    {
+      storyPoint: "3",
+      votes: 3,
+    },
+    {
+      storyPoint: "5",
+      votes: 2,
+    },
+    {
+      storyPoint: "?",
+      votes: 2,
+    },
+  ];
   return (
     <div>
       <LandingPageHeader roundedHeaderButton={false} />
       <VoteViewer />
       <div className="fixed bottom-0 w-full">
         {/*<CardSelector />*/}
-        <ResultViewer />
+        <VotingResult winners={winners} />
       </div>
     </div>
   );
---
+++ src/apps/MainApp/VotingResult.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+export interface VotingResultProps {
+  winners: Array<{
+    storyPoint: string;
+    votes: number;
+  }>;
+}
+
+export const VotingResult: React.FC<VotingResultProps> = ({ winners }) => {
+  return (
+    <div className=" bg-brand-blue p-4">
+      <div className="flex space-x-3 justify-center">
+        {winners.map((card) => {
+          return (
+            <div>
+              <div className="w-32 h-46  rounded text-center shadow bg-brand-yellow  text-white cursor-pointer">
+                <p className="py-16 font-bold text-6xl ">{card.storyPoint}</p>
+              </div>
+              <div className="text-white text-center py-2 font-bold">
+                Votes: {card.votes}
+              </div>
+            </div>
+          );
+        })}
+        <div>
+          <div className="w-auto h-46 flex  rounded text-center shadow   bg-gray-50 text-brand-green-dark cursor-pointer">
+            <div className="px-14 py-16 font-bold">
+              <div className="text-4xl">0</div>
+              <div className="text-sm">Average</div>
+            </div>
+            <div className="w-0.5 h-32 my-6 bg-gray-300"></div>
+            <div className="px-12 py-16 font-bold">
+              <div className="text-3xl">100%</div>
+              <div className="text-sm">Agreement</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
